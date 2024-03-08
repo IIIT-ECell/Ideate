5cff4ae6eb0b4a998a770a03c9f3eb797688e974
PraneethJain <praneethjain005@gmail.com>
2024-03-08 09:38:14 +0530

    feat: increase hover shadow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,7 +24,7 @@ const config: Config = {
           const color1 = colors[color]["500"];
           const color2 = colors[color]["700"];
           neonUtilities[`.neon-${color}`] = {
-            boxShadow: `0 0 5px ${color1}, 0 0 20px ${color2}`,
+            boxShadow: `0 0 16px ${color1}, 0 0 24px ${color2}`,
           };
         }
       }
